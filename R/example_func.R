6ebdbaa2ad192df69dc0e749fb34099bda7434c2
Gotthelf <GOTTHMA2@novartis.net>
2023-10-24 17:05:40 +0200

    added

---
+++ R/example_func.R
@@ -0,0 +1,8 @@
+# Here you can create files with special custom functions that you maybe
+# use several places. Like both in different scripts and docs.
+# That way you dont have to repeat your self!
+# access these files from anywhere in your project with
+# here("R/example_func.R")
+my_function <- function(x){
+  cat(x)
+}
